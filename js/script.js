f605a552c711399e3258180d77373b960c64c474
asplundoo <oliver@asplund.net>
2023-05-04 12:54:08 +0200

    90%

--- js/script.js
+++ js/script.js
@@ -1,14 +1,57 @@
-const boxActive = document.getElementsByClassName('sales')[0];
-
-window.onscroll = function() {
-    const scroll = window.pageYOffset;
-
-    if (scroll > 300) {
-        boxActive.classList.add('active');
-    }
-}
+document.addEventListener("DOMContentLoaded", () => {
+    ScrollReveal().reveal('.animate', {
+        delay: 200, distance: "200px", viewOffset: {
+            bottom: 100
+        }
+    });
+})
 
 
 
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    scrollbar: false,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
 
 
+
+
+
+
+
+const burger = document.querySelector('.burger');
+const links = document.querySelector('.links');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  links.classList.toggle('active');
+});
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+  const showBtn = dropdown.querySelector('.show');
+  const content = dropdown.querySelector('.dropdown-content');
+
+  showBtn.addEventListener('click', () => {
+    content.classList.toggle('show');
+  });
+});
